08d5733dc4d4e0f390aeeb3f82c8748db689fc0a
指尖 <zhouj201314@gmail.com>
2025-07-30 21:34:57 +0800

    Reapply "初始化"
    This reverts commit 6167ab060a38dd6830c99844c5069d6a55629577.

---
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -0,0 +1,134 @@
+package com.sky.controller.admin;
+
+import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.EmployeeDTO;
+import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
+import com.sky.entity.Employee;
+import com.sky.properties.JwtProperties;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.EmployeeService;
+import com.sky.utils.JwtUtil;
+import com.sky.vo.EmployeeLoginVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 员工管理
+ */
+@RestController
+@RequestMapping("/admin/employee")
+@Slf4j
+@Api(tags = "员工相关接口")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+    @Autowired
+    private JwtProperties jwtProperties;
+
+    /**
+     * 登录
+     *
+     * @param employeeLoginDTO
+     * @return
+     */
+    @PostMapping("/login")
+    @ApiOperation(value = "员工登录")
+    public Result<EmployeeLoginVO> login(@RequestBody EmployeeLoginDTO employeeLoginDTO) {
+        log.info("员工登录：{}", employeeLoginDTO);
+
+        Employee employee = employeeService.login(employeeLoginDTO);
+
+        //登录成功后，生成jwt令牌
+        Map<String, Object> claims = new HashMap<>();
+        claims.put(JwtClaimsConstant.EMP_ID, employee.getId());
+        String token = JwtUtil.createJWT(
+                jwtProperties.getAdminSecretKey(),
+                jwtProperties.getAdminTtl(),
+                claims);
+
+        EmployeeLoginVO employeeLoginVO = EmployeeLoginVO.builder()
+                .id(employee.getId())
+                .userName(employee.getUsername())
+                .name(employee.getName())
+                .token(token)
+                .build();
+
+        return Result.success(employeeLoginVO);
+    }
+
+    /**
+     * 退出
+     *
+     * @return
+     */
+    @PostMapping("/logout")
+    @ApiOperation(value = "员工登出")
+    public Result<String> logout() {
+        return Result.success();
+    }
+
+    /**
+     * 添加员工
+     *
+     * @param employeeDTO
+     * @return
+     */
+    @PostMapping
+    @ApiOperation(value = "添加员工")
+    //获取json数据记得加@RequestBody注解
+    public Result save(@RequestBody EmployeeDTO employeeDTO) {
+        employeeService.save(employeeDTO);
+        return Result.success();
+    }
+
+    /**
+     * 分页查询员工
+     * @param employeeDTO
+     * @return
+     */
+    @GetMapping("/page")
+    @ApiOperation("员工查询分页")
+    public Result<PageResult> page(EmployeePageQueryDTO employeePageQueryDTO) {
+        log.info("员工信息：{}", employeePageQueryDTO);
+        PageResult result = employeeService.pageQuery(employeePageQueryDTO);
+        return Result.success(result);
+    }
+
+    /**
+     * 启用、禁用员工账号
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用、禁用员工账号")
+    public Result onoff(@PathVariable Integer status, Long id){
+        employeeService.onoff(status, id);
+        return Result.success();
+    }
+
+    @GetMapping("/{id}")
+    @ApiOperation("根据id查询员工信息")
+    public Result<Employee> getById(@PathVariable Long id) {
+        Employee employee = employeeService.getById(id);
+        return Result.success(employee);
+    }
+
+    @PutMapping
+    @ApiOperation("编辑员工信息")
+    public Result setemployee(@RequestBody EmployeeDTO employeeDTO) {
+        employeeService.setemployee(employeeDTO);
+        return Result.success();
+    }
+
+
+}
---
+++ sky-server/src/main/java/com/sky/controller/admin/SetMealController.java
@@ -0,0 +1,103 @@
+package com.sky.controller.admin;
+
+import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.entity.Setmeal;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.SetMealService;
+import com.sky.vo.SetmealVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/setmeal")
+@Api(tags = "套餐相关接口")
+@Slf4j
+public class SetMealController {
+
+    @Autowired
+    private SetMealService setMealService;
+
+    /**
+     * 分页查询套餐
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    @ApiOperation("分页查询套餐")
+    @GetMapping("/page")
+    public Result<PageResult> page(SetmealPageQueryDTO setmealPageQueryDTO){
+        PageResult pageResult = setMealService.pageQuery(setmealPageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+    /**
+     * 新增菜品
+     * @param setmealDTO
+     * @return
+     */
+    @ApiOperation("新增套餐")
+    @PostMapping
+    public Result insert(@RequestBody SetmealDTO setmealDTO){
+        setMealService.insert(setmealDTO);
+        return Result.success();
+    }
+
+    /**
+     * 修改套餐售卖状态
+     * @param setmealDTO
+     * @return
+     */
+    @ApiOperation("修改套餐售卖状态")
+    @PostMapping("/status/{status}")
+    public Result status(@PathVariable Integer status, Long id){
+        log.info("状态：{}，id：{}", status, id);
+        setMealService.status(status, id);
+        return Result.success();
+    }
+
+    /**
+     * 批量删除套餐
+     * @param ids
+     * @return
+     */
+    @ApiOperation("删除套餐")
+    @DeleteMapping
+    //接口类型参数（如 List、Map）必须显式添加 @RequestParam，否则 Spring 无法实例化接口，导致构造函数异常。
+    //简单类型 / 数组参数（如 String、Integer[]）可省略 @RequestParam，但集合接口（List、Set）必须通过注解指定参数来源。
+    public Result delete(@RequestParam List<Long> ids){
+        log.info("删除套餐:{}", ids);
+        setMealService.delete(ids);
+        return Result.success();
+    }
+
+    /**
+     * 根据套餐id查询套餐，用于修改套餐
+     * @param id
+     * @return
+     */
+    @ApiOperation("根据id查询套餐")
+    @GetMapping("/{id}")
+    public Result<SetmealVO> getById(@PathVariable Long id){
+        SetmealVO setmealVO = setMealService.getById(id);
+        return Result.success(setmealVO);
+    }
+
+
+    /**
+     * 修改套餐
+     * @param setmealDTO
+     * @return
+     */
+    @ApiOperation("修改套餐")
+    @PutMapping
+    public Result update(@RequestBody SetmealDTO setmealDTO){
+        setMealService.update(setmealDTO);
+        return Result.success();
+    }
+}
